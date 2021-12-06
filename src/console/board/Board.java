83a3710bc4dd3c69509eea55c4a6b04c514d7ef3
meesvdent <meesvdent@gmail.com>
2021-12-06 12:43:40 +0100

    question parsing

--- src/console/board/Board.java
+++ src/console/board/Board.java
@@ -1,9 +1,12 @@
 package console.board;
 
+import console.questions.Question;
 import console.squares.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -15,9 +18,12 @@ public class Board {
     private AbstractSquare[][] squares;
     private AnswerSquare[] answerSquares;
 
+    private ArrayList<Question>[] questions;
+
     public Board(String filename, String answer){
         this.parseSquares(filename);
         this.parseAnswer(answer);
+        this.parseQuestions(filename);
     }
 
     public int getWidth(){
@@ -40,10 +46,18 @@ public class Board {
         StringBuilder output = new StringBuilder();
         for(int i=0; i<this.height; i++){
             for(int j=0; j<this.width; j++){
-                output.append(this.squares[i][j]);
-                output.append("\t");
+                output.append(this.squares[i][j]).append("\t");
             }
             output.append("\n");
+        }
+        output.append("Questions:\n");
+        output.append("ACROSS\n");
+        for(Object question : this.questions[0]){
+            output.append(question).append("\n");
+        }
+        output.append("DOWN\n");
+        for(Object question: this.questions[1]){
+            output.append(question).append("\n");
         }
         return output.toString();
     }
@@ -122,6 +136,40 @@ public class Board {
         }
 
         this.answerSquares = answerSquares;
+    }
+
+    public void parseQuestions(String filename){
+
+        this.questions = new ArrayList[2];
+
+        try(Scanner questionScan = new Scanner(new FileReader(filename))){
+            while (questionScan.hasNextLine()){
+                String nextLine = questionScan.nextLine();
+                Scanner nextScan = new Scanner(nextLine);
+                String nextWord = nextScan.next();
+                if(nextWord.equals("ACROSS")){
+                    this.questions[0] = new ArrayList<>();
+                    nextLine = questionScan.nextLine();
+                    nextScan = new Scanner(nextLine);
+                    nextWord = nextScan.next();
+                    while(! nextWord.equals("DOWN")){
+                        this.questions[0].add(new Question(nextLine));
+                        nextLine = questionScan.nextLine();
+                        nextScan = new Scanner(nextLine);
+                        nextWord = nextScan.next();
+                    }
+                }
+                if (nextWord.equals("DOWN")) {
+                    this.questions[1] = new ArrayList<>();
+                    while (questionScan.hasNextLine()){
+                        this.questions[1].add(new Question(questionScan.nextLine()));
+                    }
+                }
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean checkAnswer(){
---
+++ src/console/questions/Question.java
@@ -0,0 +1,14 @@
+package console.questions;
+
+public class Question {
+
+    private String question;
+
+    public Question(String question){
+        this.question = question;
+    }
+
+    public String toString(){
+        return this.question;
+    }
+}
